0e9e31c3261e627c0a772695a835a9d1d22d1b42
Vincent Miller <vrwmiller@gmail.com>
2025-10-26 22:33:13 -0400

    Update myopen.sh to check both /Applications and /System/Applications for app executables

--- myopen.sh
+++ myopen.sh
@@ -2,12 +2,16 @@
 # Usage: ./myopen.sh <AppName>
 # Example: ./myopen.sh Calculator
 
+
 APP_NAME="$1"
 APP_PATH="/Applications/$APP_NAME.app/Contents/MacOS/$APP_NAME"
+SYS_APP_PATH="/System/Applications/$APP_NAME.app/Contents/MacOS/$APP_NAME"
 
-if [ ! -x "$APP_PATH" ]; then
-  echo "Cannot find executable for $APP_NAME at $APP_PATH"
+if [ -x "$APP_PATH" ]; then
+  open -n "$APP_PATH" &
+elif [ -x "$SYS_APP_PATH" ]; then
+  open -n "$SYS_APP_PATH" &
+else
+  echo "Cannot find executable for $APP_NAME at $APP_PATH or $SYS_APP_PATH"
   exit 1
 fi
-
-open -n "$APP_PATH" &
