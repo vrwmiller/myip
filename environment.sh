91dfd8220e871a5e48ef06797bc30aa56d03a74c
Vincent Miller <vrwmiller@gmail.com>
2025-10-26 21:47:07 -0400

    Add bandsintown alias to environment.sh

--- environment.sh
+++ environment.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 # Create and activate Python virtual environment, then install dependencies
-python3 -m venv venv
+/opt/homebrew/bin/python3.13 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
 # Alias to run the tool easily
 alias myip='python myip.py'
+alias bandsintown='python bandsintown.py'
